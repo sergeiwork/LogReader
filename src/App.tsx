ed7dd51307f8efbd6fa2bfd717411721a2d84ec8
Sergei <sergeiwork@yahoo.com>
2020-12-31 01:25:53 +0300

    Add exceptions table Add workers table Add Level styles Add Properties to LogLine model

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React, { createRef, useState } from "react";
 import "./App.css";
 import { Button, Table } from "reactstrap";
 
+interface ILogLineProperties {
+  SourceContext: string;
+  ElapsedMilliseconds: number;
+  MetricName: string;
+  WorkItemKey: string;
+  ConcurrentTaskIndex: number;
+  WorkerSessionId: string;
+  WorkerName: string;
+  AssemblyName: string;
+  AssemblyVersion: string;
+  MachineName: string;
+  EnvironmentUserName: string;
+  ApplicationSessionId: string;
+}
+
 class LogLine {
-  Timestamp: string = "";
+  Timestamp: Date = new Date();
   Level: string = "";
   Message: string = "";
   Exception: string = "";
+  Id: number = 0;
+  Properties: ILogLineProperties | null = null;
 }
 
 const format = (str: string, obj: any): string => {
@@ -20,49 +37,110 @@ const format = (str: string, obj: any): string => {
 function App() {
   const fileInput = createRef<HTMLInputElement>();
   const [logLines, setLogLines] = useState<LogLine[]>([]);
+  const [exceptions, setExceptions] = useState(new Map<string, number>());
+  const [workers, setworkers] = useState(new Map<string, number>());
   const loadFile = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     const reader = new FileReader();
     reader.addEventListener("load", (event) => {
       const lines = (event.target?.result as string).split("\n");
-      setLogLines(
-        lines.map((l) => {
-          const obj = JSON.parse(l);
-          return {
-            Timestamp: obj.Timestamp,
-            Level: obj.Level,
-            Message: format(obj.MessageTemplate, obj.Properties),
-            Exception: obj.Exception,
-          };
-        })
-      );
+      var objects = lines.map<LogLine>((l, i) => {
+        const obj = JSON.parse(l);
+        return {
+          Timestamp: new Date(obj.Timestamp),
+          Level: obj.Level,
+          Message: format(obj.MessageTemplate, obj.Properties),
+          Exception: obj.Exception,
+          Id: i,
+          Properties: obj.Properties,
+        };
+      });
+      const newExceptions = new Map<string, number>();
+      for (let log of objects) {
+        if (log.Exception) {
+          let exception = log.Exception.split(" ")[0];
+          if (newExceptions.has(exception))
+            newExceptions.set(exception, newExceptions.get(exception)! + 1);
+          else newExceptions.set(exception, 1);
+        }
+      }
+      setExceptions(newExceptions);
+      const newWorkers = new Map<string, number>();
+      for (let log of objects) {
+        if (log.Properties?.WorkerName) {
+          let worker = log.Properties.WorkerName + " " + log.Level;
+          if (newWorkers.has(worker))
+            newWorkers.set(worker, newWorkers.get(worker)! + 1);
+          else newWorkers.set(worker, 1);
+        }
+      }
+      setworkers(newWorkers);
+      setLogLines(objects);
     });
     reader.readAsText(fileInput.current!.files![0]);
   };
 
   return (
     <div className="app">
+      <input type="file" ref={fileInput} accept=".json" />
+      <Button onClick={loadFile}>Load</Button>
       <Table>
         <thead>
           <tr>
-            <td>Time</td>
-            <td>Level</td>
-            <td>Message</td>
-            <td>Exception</td>
+            <td width="10%">Count</td>
+            <td width="auto">Exception</td>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(exceptions.keys())
+            .sort()
+            .map((k) => (
+              <tr key={k}>
+                <td>{exceptions.get(k)}</td>
+                <td>{k}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+      <Table>
+        <thead>
+          <tr>
+            <td width="10%">Count</td>
+            <td width="auto">Worker</td>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(workers.keys())
+            .sort()
+            .map((k) => (
+              <tr key={k}>
+                <td>{workers.get(k)}</td>
+                <td>{k}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+      <Table>
+        <thead>
+          <tr>
+            <td width="10%">Time</td>
+            <td width="10%">Level</td>
+            <td width="10%">Work Item</td>
+            <td width="20%">Message</td>
+            <td width="auto">Exception</td>
           </tr>
         </thead>
         <tbody>
           {logLines.map((l) => (
-            <tr>
-              <td>{l.Timestamp}</td>
+            <tr key={l.Id} className={"logRow " + l.Level}>
+              <td>{l.Timestamp.toLocaleString()}</td>
               <td>{l.Level}</td>
+              <td>{l.Properties?.WorkItemKey ?? "*"}</td>
               <td>{l.Message}</td>
               <td>{l.Exception}</td>
             </tr>
           ))}
         </tbody>
       </Table>
-      <input type="file" ref={fileInput} accept=".json" />
-      <Button onClick={loadFile}>Load</Button>
     </div>
   );
 }
